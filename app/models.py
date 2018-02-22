b8538e2274ccf031e4b3f0f2dba7fb74cb2b5420
Colin Grodecoeur <clngrdcr@gmail.com>
2018-02-22 21:22:24 +0100

    Fixed participant draw bug (error 500 when the draw is not filled)

--- app/models.py
+++ app/models.py
@@ -417,6 +417,10 @@ class Participant(db.Model):
     def has_filled_draw(self):
         return len(self.forecasts.all()) > 0
 
+
+    def has_completely_filled_draw(self):
+        pass
+
     def get_score(self):
         score_per_round = self.tournament.get_score_per_round()
         score = 0
