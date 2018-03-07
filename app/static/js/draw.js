ed60c97c896eda5c0097be194217011c77620bb9
Colin Grodecoeur <clngrdcr@gmail.com>
2018-03-07 21:59:47 +0100

    En tant que participant, quand je remplis mon tableau, je ne pas faire de prono illogique

--- app/static/js/draw.js
+++ app/static/js/draw.js
@@ -22,10 +22,34 @@ function fill_next_round(event, match_position, player_position) {
 
         // fill tournament_player_id attribute with origin tournament_player_id
         $(destination_id).attr("tournament_player_id", $(origin_id).attr("tournament_player_id"));
+
+        // find other player id
+        if (player_position == "0") {
+            other_player_id = "#player-" + match_position + "-1";
+        } else {
+            other_player_id = "#player-" + match_position + "-0";
+        }
+
+        // Get corresponding tournament player id
+        other_tournament_player_id = $(other_player_id).attr("tournament_player_id")
+        $(".game-player").each(function() {
+            if ($(this).attr("tournament_player_id") == other_tournament_player_id
+                & $(this).attr("match_position_id") < match_position) {
+                $(this).attr("tournament_player_id", "None");
+                $(this).text("\xa0");
+                console.log($(this).attr("match_position_id"))
+            }
+        })
     }
 
     update_json();
 }
+
+match_position=3
+player_position=0
+origin_id = "#player-" + match_position + "-" + player_position;
+other_player_id = "#player-" + match_position + "-1"
+other_tournament_player_id = $(other_player_id).attr("tournament_player_id")
 
 function update_json() {
     var res = {};
