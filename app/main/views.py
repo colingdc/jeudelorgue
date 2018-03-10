38591df05ce32f9da74761761c7c8cb9c2f4f774
Colin Grodecoeur <clngrdcr@gmail.com>
2018-03-10 22:54:49 +0100

    Redirect logged in users to index on homepage visit

--- app/main/views.py
+++ app/main/views.py
@@ -15,6 +15,8 @@ from flask import current_app
 @bp.route("/")
 def landing():
     title = "Accueil"
+    if current_user.is_authenticated:
+      return redirect(url_for(".index"))
     return render_template("main/homepage.html", title = title)
 
 
