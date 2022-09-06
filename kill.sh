7c30adf4b5e5573c48068f0b806da8403c561dfa
colingdc <colin.grodecoeur@gmail.com>
2022-09-06 23:08:28 +0200

    Remove launch script
    They are env-specific

--- kill.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-for pid in $(ps aux | grep gunicorn | grep 8001 | awk '{print $2}'); do kill ${pid}; done
-
--- launch.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-nohup gunicorn manage:app -b localhost:8001 >/dev/null &
-
