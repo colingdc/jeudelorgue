8cc8a4a53624069da5d39318aacc6f2c3b66b506
colingdc <colin.grodecoeur@gmail.com>
2022-10-10 02:31:15 +0200

    Replace get_all_surface and get_all_categories methods

--- app/tournament/domain/get_surfaces.py
+++ app/tournament/domain/get_surfaces.py
@@ -2,4 +2,7 @@ from ...models import Surface
 
 
 def get_surfaces():
-    return Surface.get_all_surfaces()
+    return [
+        (surface.id, surface.name)
+        for surface in Surface.query.order_by(Surface.name).all()
+    ]
