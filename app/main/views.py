5e184dc81ef0c6c2f60113a85b8325e80d6616f0
colingdc <colin.grodecoeur@gmail.com>
2025-08-09 13:36:49 +0200

    Remove unused route

--- app/main/views.py
+++ app/main/views.py
@@ -212,21 +212,3 @@ def contact():
         form=form,
         title=title
     )
-
-
-@bp.route("/test")
-def test(chartID='chart_ID', chart_type='bar', chart_height=350):
-    chart = {"renderTo": chartID, "type": chart_type, "height": chart_height, }
-    series = [{"name": 'Label1', "data": [1, 2, 3]}, {"name": 'Label2', "data": [4, 5, 6]}]
-    title = {"text": 'My Title'}
-    xAxis = {"categories": ['xAxis Data1', 'xAxis Data2', 'xAxis Data3']}
-    yAxis = {"title": {"text": 'yAxis Label'}}
-    return render_template(
-        'main/test.html',
-        chartID=chartID,
-        chart=chart,
-        series=series,
-        title=title,
-        xAxis=xAxis,
-        yAxis=yAxis
-    )
