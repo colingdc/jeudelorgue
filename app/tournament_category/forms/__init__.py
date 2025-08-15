9c06bba54a43abfc74d28cdc084a371164aa0599
colingdc <colin.grodecoeur@gmail.com>
2025-08-15 16:15:47 +0200

    Split tournament_category forms

---
+++ app/tournament_category/forms/__init__.py
@@ -0,0 +1,2 @@
+from .create_category import CreateCategoryForm
+from .edit_category import EditCategoryForm
---
+++ app/tournament_category/forms/create_category.py
@@ -0,0 +1,41 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, IntegerField
+from wtforms.validators import DataRequired
+
+from ...lang import WORDINGS
+from ...models import TournamentCategory
+
+
+class CreateCategoryForm(FlaskForm):
+    name = StringField(
+        WORDINGS.TOURNAMENT.NAME,
+        validators=[
+            DataRequired(message=WORDINGS.COMMON.MISSING_FIELD)
+        ]
+    )
+    number_rounds = IntegerField(
+        WORDINGS.TOURNAMENT.NUMBER_OF_ROUNDS,
+        validators=[
+            DataRequired(message=WORDINGS.COMMON.MISSING_FIELD)
+        ]
+    )
+    maximal_score = IntegerField(
+        WORDINGS.TOURNAMENT.NUMBER_OF_POINTS_MAX,
+        validators=[
+            DataRequired(message=WORDINGS.COMMON.MISSING_FIELD)
+        ]
+    )
+    minimal_score = IntegerField(
+        validators=[
+            DataRequired(message=WORDINGS.COMMON.MISSING_FIELD)
+        ]
+    )
+
+    def validate(self, extra_validators=None):
+        rv = FlaskForm.validate(self, extra_validators)
+        if not rv:
+            return False
+        if (TournamentCategory.query.filter_by(name=self.name.data).first()):
+            self.name.errors.append(WORDINGS.TOURNAMENT.CATEGORY_ALREADY_EXISTS)
+            return False
+        return True 
