bf82c50cba95bd90287170affcf73669b82b8231
colingdc <colin.grodecoeur@gmail.com>
2025-08-16 18:02:48 +0200

    Remove useless title
    it didn't work, because the page doesn't use the common layout

--- app/tournament/views/raw_tournament_ranking.py
+++ app/tournament/views/raw_tournament_ranking.py
@@ -3,7 +3,6 @@ from flask import render_template
 from .. import bp
 from .. import domain
 from ...decorators import manager_required
-from ...lang import WORDINGS
 
 
 @bp.route("/<tournament_id>/ranking/raw")
@@ -11,10 +10,7 @@ from ...lang import WORDINGS
 def raw_tournament_ranking(tournament_id):
     tournament = domain.get_tournament(tournament_id)
 
-    title = WORDINGS.RANKING.RAW_RANKING.format(tournament.name)
-
     return render_template(
         "tournament/raw_tournament_ranking.html",
-        title=title,
         tournament=tournament
     ) 
