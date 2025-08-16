cfef42b2429aa723ef25ebb40cafc38bdfb819a8
colingdc <colin.grodecoeur@gmail.com>
2025-08-16 21:27:39 +0200

    Simplify scenario simulator

--- app/tournament/views/scenario_simulator.py
+++ app/tournament/views/scenario_simulator.py
@@ -19,52 +19,31 @@ def scenario_simulator(tournament_id):
         return routing.redirect_to_view_tournament(tournament_id)
 
     form = FillTournamentDrawForm()
+    scenario = {match.id: match.winner_id or "None" for match in tournament.matches}
 
     if request.method == "GET":
-        scenario = {match.id: match.winner_id or "None" for match in tournament.matches}
         form.forecast.data = json.dumps(scenario)
-        scenario = {int(k): (int(v) if v != "None" else None) for k, v in scenario.items()}
-
-        scores = [(participant, participant.get_score_simulator(scenario), participant.risk_coefficient) for participant
-                  in tournament.participants]
-        scores = sorted(scores, key=lambda x: (-x[1], -x[2]))
-
-        simulated_matches = {match_id: None if winner_id is None else TournamentPlayer.query.get(winner_id) for
-                             match_id, winner_id in scenario.items()}
-
-    if form.validate_on_submit():
+    elif form.validate_on_submit():
         try:
             results = json.loads(form.forecast.data)
+            scenario = {int(k): (int(v) if v != "None" else None) for k, v in results.items()}
         except json.decoder.JSONDecodeError:
             return routing.redirect_to_view_tournament(tournament_id)
-        scenario = {int(k): (int(v) if v != "None" else None) for k, v in results.items()}
 
-        scores = [(participant, participant.get_score_simulator(scenario), participant.risk_coefficient) for participant
-                  in tournament.participants]
-        scores = sorted(scores, key=lambda x: (-x[1], -x[2]))
+    scores = [(participant, participant.get_score_simulator(scenario), participant.risk_coefficient) for participant
+              in tournament.participants]
+    scores = sorted(scores, key=lambda x: (-x[1], -x[2]))
 
-        simulated_matches = {match_id: None if winner_id is None else TournamentPlayer.query.get(winner_id) for
-                             match_id, winner_id in scenario.items()}
+    simulated_matches = {match_id: None if winner_id is None else TournamentPlayer.query.get(winner_id) for
+                         match_id, winner_id in scenario.items()}
 
-        return render_template(
-            "tournament/scenario_simulator.html",
-            title=tournament.name,
-            tournament=tournament,
-            form=form,
-            scenario=scenario,
-            scores=scores,
-            simulated_matches=simulated_matches,
-            surface=tournament.surface.class_name
-        )
-
-    else:
-        return render_template(
-            "tournament/scenario_simulator.html",
-            title=tournament.name,
-            tournament=tournament,
-            form=form,
-            scenario=scenario,
-            scores=scores,
-            simulated_matches=simulated_matches,
-            surface=tournament.surface.class_name
-        ) 
+    return render_template(
+        "tournament/scenario_simulator.html",
+        title=tournament.name,
+        tournament=tournament,
+        form=form,
+        scenario=scenario,
+        scores=scores,
+        simulated_matches=simulated_matches,
+        surface=tournament.surface.class_name
+    )
